f7822e1822c79394cf29b0f0d1815bd6338e089d
Paras Pahwa <134779342+paraspahwa@users.noreply.github.com>
2026-02-27 19:36:31 +0000

    updated lib file

--- user_data.sh
+++ user_data.sh
@@ -5,3 +5,5 @@ sudo apt install jdk-21-openjdk -y
 sudo apt update -y
 sudo apt install mysql-server -y
 sudo service mysql start
+sudo mkdir -p /var/lib/mysql
+sudo chown mysql:mysql /var/lib/mysql
